a10560be0a86f3c0275915a20912ba70f212ec0d
Pierre Gibertini <pierre.gibertini@gmail.com>
2024-10-08 10:21:03 +0200

    Remove useless encoding

--- package_foler/folder/__init__.py
+++ package_foler/folder/__init__.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 {{PROJECT_NAME}} Package
 """
-
-import pkg_resources
-
-__version__ = pkg_resources.require("{{PACKAGE_NAME}}")[0].version
--- package_foler/tests/test_exemple.py
+++ package_foler/tests/test_exemple.py
@@ -1,12 +1,13 @@
 import unittest
-import numpy as npy
+import numpy as np
 
 
 class TestClassName(unittest.TestCase):
 
     def test_method_name(self):
         results = [0, 3, 0]
-        self.assertTrue(npy.all(results == npy.array([0, 3, 0])))
+        self.assertTrue(np.all(results == np.array([0, 3, 0])))
+
 
 if __name__ == "__main__":
     unittest.main()
